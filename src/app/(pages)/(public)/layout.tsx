26f57ecad79ebb1c81ea18d375806fc6c78673f6
popsy-pops <xcute.official@gmail.com>
2025-01-16 14:11:13 -0500

    [developing Notice page]: being developed home page done

---
+++ src/app/(pages)/(public)/layout.tsx
@@ -0,0 +1,10 @@
+
+const layout = ({children}:{children: React.ReactNode;}) => {
+  return (
+    <div className="">
+        {children}
+    </div>
+  )
+};
+
+export default layout;
--- src/app/(pages)/(public)/page.tsx
+++ src/app/(pages)/(public)/page.tsx
@@ -1,8 +1,14 @@
-
+import Image from "next/image";
 const page = () => {
   return (
-    <div>
-        <h1>whoami@nouser:~$</h1>
+    <div className="flex items-center justify-center w-screen h-screen font-mono text-xs px-16">
+      <div className="flex flex-col-reverse gap-16 md:flex-row w-full md:w-1/2 md:justify-between">
+        <div className="flex flex-col gap-4 justify-center w-fit">
+          <h1 className="text-center"><span className="text-green-600">whoami</span>@nouser:~$</h1>
+          <span className="p-2 rounded-md border-red-500 bg-transparent border text-red-500 w-fit mx-auto">website is being developed</span>
+        </div>
+        <Image src={`/imgs/developer.png`} alt="image-error" height={200} width={200} />
+      </div>
     </div>
   )
 };
